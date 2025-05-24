ac2a49e954efd850a5ed740db53f5aec0fadd6c8
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-24 14:07:11 -0300

    Start the logic for use input grabbing

---
+++ OpenGL/windowEvents.cpp
@@ -0,0 +1,17 @@
+#include "window.hpp"
+
+#define KEY_PRESS_EVENT		0
+#define KEY_RELEASE_EVENT	1
+
+void Window::handleInput(int mode) {
+
+	// Handle when a key is pressed down
+	if (mode == KEY_PRESS_EVENT) {
+
+		// Apply the logic for the used keys
+		switch (this->windowInfo.mainWindowEvent.key.key) {
+		
+		}
+	}
+	
+}
