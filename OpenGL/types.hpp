a6824a45148f946c99e0ef72fe0bd275ea8a1cd2
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-24 14:23:46 -0300

    Adapt the custom data types to be more in the C++ standard

--- OpenGL/types.hpp
+++ OpenGL/types.hpp
@@ -1,14 +1,16 @@
 #pragma once
 #include <SDL3/SDL.h>
 
-typedef struct SDL_WindowInfo {
+using SDL_WindowInfo = struct SDL_WindowInfo;
+
+struct SDL_WindowInfo {
 	SDL_Window* mainWindowPtr;
 	SDL_Renderer* mainWindowRenderer;
 	SDL_Event		mainWindowEvent;
 	SDL_GLContext	mainWindowGLContext;
-} SDL_WindowInfo;
+};
 
-typedef struct GL_InfoStruct {
+struct GL_InfoStruct {
 	int viewPortWidth;
 	int viewPortHeight;
-} GL_InfoStruct;
+};
