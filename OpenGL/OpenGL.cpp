87c963e2eae079462a3adc3e73f110c6463e5a89
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-24 15:51:40 -0300

    Create a function to handle with the OpenGL viewport resizing

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -4,3 +4,12 @@
 
 #include "window.hpp"
 
+void Window::resizeGLViewport() {
+	SDL_GetWindowSize(this->windowInfo.mainWindowPtr, &this->windowWidth, &this->windowHeight);
+	
+	// Cast the dimension to GLsizei just to make sure there won't be any problem
+	GLsizei width  = static_cast<GLsizei>(this->windowWidth);
+	GLsizei height = static_cast<GLsizei>(this->windowHeight);
+
+	glViewport(0, 0, width, height);
+}
