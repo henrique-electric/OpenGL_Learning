672c59a2c82a9f208413fd1bb9eb32ea413dae7d
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-24 15:52:12 -0300

    Remove useless struct

--- OpenGL/types.hpp
+++ OpenGL/types.hpp
@@ -1,16 +1,13 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <glad/glad.h>
 
 using SDL_WindowInfo = struct SDL_WindowInfo;
+using GL_InfoStruct = struct GL_InfoStruct;
 
 struct SDL_WindowInfo {
 	SDL_Window* mainWindowPtr;
 	SDL_Renderer* mainWindowRenderer;
 	SDL_Event		mainWindowEvent;
 	SDL_GLContext	mainWindowGLContext;
-};
-
-struct GL_InfoStruct {
-	int viewPortWidth;
-	int viewPortHeight;
 };
