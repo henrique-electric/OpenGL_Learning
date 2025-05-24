2770c259ffbb18ab0c842f65c665221604efa916
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-24 02:27:06 -0300

    Test SDL

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,30 +1,21 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "window.hpp"
 
-int main(void) {
-// Initialize the library
-	if (!glfwInit())
-		return -1;
-	// Create a windowed mode window and its OpenGL context
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
-		return -1;
-	}
-	// Make the window's context current
-	glfwMakeContextCurrent(window);
-	// Initialize GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		return -1;
+int main(void) {	
+	
+	Window win = Window("App", 1280, 720);
+
+	if (win.initStatus != 0) {
+		win.destroy();
+		return win.initStatus;
 	}
 
-	// Loop until the user closes the window
-	while (!glfwWindowShouldClose(window)) {
-		glfwSwapBuffers(window);
+	while (!win.shouldClose()) {
+		while (SDL_PollEvent(win.getEventRef())) {
+			win.eventHandler();
+		}
 
-		glfwPollEvents();
+		win.update();
 	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
+
 	return 0;
 }
