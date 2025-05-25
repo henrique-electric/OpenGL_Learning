fbb6ba013eb378110e35c18526e843dce64dd7ab
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-25 18:48:20 -0300

    Make the Window class inherit the GL_Backend class in order to have access to the OpenGL abstraction functions

--- OpenGL/include/window.hpp
+++ OpenGL/include/window.hpp
@@ -2,6 +2,7 @@
 // STD includes
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 // SDL, OpenGL and external includes
 #include <SDL3/SDL.h>
@@ -11,13 +12,14 @@
 // Header files
 #include <erros.hpp>   // Include the error enumaration 
 #include <types.hpp>   // Contains custom data type structs
+#include <OpenGL.hpp>
 
 enum {
 	KEY_PRESS_EVENT,
 	KEY_RELEASE_EVENT,
 };
 
-class Window {
+class Window : public GL_Backend{
 public:
 	// Window class methods
 	Window(std::string windowTitle, int width, int height);
