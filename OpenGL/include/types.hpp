123c530e6a3bf4469f2db91ef017114f9059f0c9
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-25 12:07:26 -0300

    Create a struct to store info about OpenGL buffers

--- OpenGL/include/types.hpp
+++ OpenGL/include/types.hpp
@@ -3,11 +3,28 @@
 #include <glad/glad.h>
 
 using SDL_WindowInfo = struct SDL_WindowInfo;
-using GL_InfoStruct = struct GL_InfoStruct;
+using GL_InfoStruct  = struct GL_InfoStruct;
+using bufferStruct   = struct bufferStruct;
+
+using uint = unsigned int;
 
 struct SDL_WindowInfo {
 	SDL_Window* mainWindowPtr;
 	SDL_Renderer* mainWindowRenderer;
 	SDL_Event		mainWindowEvent;
 	SDL_GLContext	mainWindowGLContext;
+};
+
+/* 
+	A struct to hold a variable to store the id of the buffer
+	and also a name to make easier to identify
+*/
+struct bufferStruct {
+	std::string name;
+	uint		id;
+	GLuint		buffType;
+};
+
+enum {
+	RAW_ARRAY_GL_BUFFER
 };
--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -9,10 +9,23 @@ int main(void) {
 		return win.getInitStatus();
 	}
 
-	while (!win.shouldClose()) {
-		win.eventHandler();
+	float vertexes[] = {
+	   -0.5f,  -0.5f,  0.0f,
+		0.5f,  -0.5f,  0.0f,
+		0.0f,   0.5f,  0.0f
+	};
 
-		win.update();
+	while (!win.shouldClose()) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		win.eventHandler();
+		glBegin(GL_TRIANGLES);
+		glVertex2d(-0.5f, -0.5f);
+		glVertex2d(0.0f, 0.5f);
+		glVertex2d(0.5f, -0.5f);
+		glEnd();
+		
+		//SDL_GL_SwapWindow(win.windowInfo.mainWindowPtr);
+		//win.update();
 	}
 
 	return 0;
