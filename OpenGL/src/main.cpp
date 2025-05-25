b4da7012de7742248eaf1b87e9204746108d7e31
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-25 16:04:35 -0300

    Fix some VS bug

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -16,16 +16,9 @@ int main(void) {
 	};
 
 	while (!win.shouldClose()) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		win.eventHandler();
-		glBegin(GL_TRIANGLES);
-		glVertex2d(-0.5f, -0.5f);
-		glVertex2d(0.0f, 0.5f);
-		glVertex2d(0.5f, -0.5f);
-		glEnd();
 		
-		//SDL_GL_SwapWindow(win.windowInfo.mainWindowPtr);
-		//win.update();
+		win.eventHandler();
+		win.update();
 	}
 
 	return 0;
