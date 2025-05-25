5b4a5eb67bd9424b0afb30db52b1cf15993eb14d
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-25 18:50:27 -0300

    Add some more comments and create a struct to store the data related to a vertex

--- OpenGL/include/types.hpp
+++ OpenGL/include/types.hpp
@@ -2,18 +2,31 @@
 #include <SDL3/SDL.h>
 #include <glad/glad.h>
 
+// Typedefs for structs
 using SDL_WindowInfo = struct SDL_WindowInfo;
 using GL_InfoStruct  = struct GL_InfoStruct;
 using bufferStruct   = struct bufferStruct;
+using GL_Vertex		 = struct GL_Vertex;
 
+// Typedefs for data types
 using uint = unsigned int;
 
+
+// Structs related to the window and SDL
+
+/*
+	A struct to hold Critical info to the SDL info
+*/
 struct SDL_WindowInfo {
-	SDL_Window* mainWindowPtr;
-	SDL_Renderer* mainWindowRenderer;
+	SDL_Window	   *mainWindowPtr;
+	SDL_Renderer   *mainWindowRenderer;
 	SDL_Event		mainWindowEvent;
 	SDL_GLContext	mainWindowGLContext;
 };
+
+
+
+// structs related to OpenGL and the GL_Backend class
 
 /* 
 	A struct to hold a variable to store the id of the buffer
@@ -23,6 +36,14 @@ struct bufferStruct {
 	std::string name;
 	uint		id;
 	GLuint		buffType;
+
+};
+
+
+struct GL_Vertex {
+	float* buff; // buffer containing the data related to a vertex
+	size_t buffSize;
+
 };
 
 enum {
