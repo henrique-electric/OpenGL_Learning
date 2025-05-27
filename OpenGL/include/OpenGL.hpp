e326a5bde1f5e1c014fbb106b0a84663dd3f4d81
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-05-27 18:38:38 -0300

    declare new methods to abstract OpenGL

--- OpenGL/include/OpenGL.hpp
+++ OpenGL/include/OpenGL.hpp
@@ -1,5 +1,12 @@
 #pragma once
-#include <window.hpp>
+
+// STD includes
+#include <fstream>
+#include <vector>
+
+// Header files
+#include <types.hpp>
+#include <erros.hpp>
 
 /*
 	Class that contains the OpenGL abstractions and wrapped functions
@@ -9,10 +16,14 @@ class GL_Backend
 {
 public:
 	void createBuffer(std::string name, int type);
-	int sendData(std::string buffName,void* databuff, size_t size);
+	int	 deleteBuffer(std::string buffName);
+	int  sendData(std::string buffName,void* databuff, size_t size);
+	void newVertex(float* pos, size_t size);
 
 
 private:
+	std::ifstream fileStreaming; // used to read shader files
+
 	/*
 		Vectors to store different OpenGL objects
 	*/
